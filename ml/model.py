2c985d0ac2433159786945298665848285168cd9
Diego Amicabile <diego.amicabile@gmail.com>
2022-08-20 16:37:07 +0200

    More experiments in classification

--- ml/model.py
+++ ml/model.py
@@ -1,12 +1,12 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.ensemble import HistGradientBoostingClassifier
 import warnings
 
 
 # Optional: implement hyperparameter tuning.
-def train_model(X_train, y_train):
+def rtf_train_model(X_train, y_train):
     """
     Trains a machine learning model and returns it.
 
@@ -40,7 +40,48 @@ def train_model(X_train, y_train):
         model = rfc_cv.fit(X_train, y_train)
         return model
 
+def train_model(X_train, y_train):
+    return xgb_train_model(X_train, y_train)
 
+# Optional: implement hyperparameter tuning.
+def xgb_train_model(X_train, y_train):
+    """
+    Trains a machine learning model and returns it.
+
+    Inputs
+    ------
+    X_train : np.array
+        Training data.
+    y_train : np.array
+        Labels.
+    Returns
+    -------
+    model
+        Trained machine learning model.
+    """
+
+    seed = 42
+    param_grid = {
+        'max_iter': [750, 1000, 1200, 1500],
+        'learning_rate': [0.01, 0.1, 1],
+        'max_depth': [20, 25, 30],
+        'max_leaf_nodes': [10, 30, 50],
+        'l2_regularization': [0, 0.5, 1, 1.5, 2],
+        'scoring': ['f1_micro', 'loss'],
+        'min_samples_leaf': [10, 15, 20]
+    }
+
+    with warnings.catch_warnings():
+        # ignore all caught warnings
+        warnings.filterwarnings("ignore")
+        hgbc = HistGradientBoostingClassifier(random_state=seed)
+        CV_hgbc = RandomizedSearchCV(estimator=hgbc, param_grid=param_grid, cv=5)
+        CV_hgbc.fit(X_train, y_train)
+        best_params = CV_hgbc.best_params_
+        print(f"Found best params: {best_params}")
+        hgbc_cv = HistGradientBoostingClassifier(random_state=42, **best_params )
+        model = hgbc_cv.fit(X_train, y_train)
+        return model
 
 
 def compute_model_metrics(y, preds):
