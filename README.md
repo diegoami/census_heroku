23129500968a0bfb923b5ddc33957eb007a94756
Diego Amicabile <diego.amicabile@gmail.com>
2022-12-07 20:46:09 +0100

    Fixed api and remote test

--- README.md
+++ README.md
@@ -85,11 +85,11 @@ To ensure deployment on Heroku, two workflows have been setup,
 * _deploy_ deploys the model to heroku. The model deployed is the one in the directory `model/latest`
 
 
-It deploys to a different application (`atlantic-shell-34953`)
+It deploys to a different application (`ionian-otter-6666`)
 After deployment, a test can be executed to verify that the Remote API works correctly
 
 ```
-TEST_REMOTE_API=1 HOST=pacific-garden-34952.herokuapp.com python -m pytest test/test_remote_census_api.py
+TEST_REMOTE_API=1 HOST=ionian-otter-6666.herokuapp.com python -m pytest test/test_remote_census_api.py
 TEST_REMOTE_API=1 HOST=atlantic-shell-34953.herokuapp.com python -m pytest test/test_remote_census_api.py
 ```
 
--- test/test_remote_census_api.py
+++ test/test_remote_census_api.py
@@ -2,7 +2,8 @@ import pytest
 import requests
 import os
 
-from common.fixtures import census_entries, wrong_census_entries
+from common.fixtures import census_entries, wrong_census_entries, generate_entries
+from common.fixtures import generate_entry
 
 @pytest.fixture
 def host():
@@ -16,10 +17,9 @@ def port():
 @pytest.mark.skipif(os.environ.get("TEST_REMOTE_API") == None, reason="TEST_REMOTE_API not defined")
 def test_predict(census_entries, host, port):
     for census_entry in census_entries:
-        response = requests.post(f'https://pacific-garden-34952.herokuapp.com/predict', json=census_entry)
+        response = requests.post(f'http://{host}:{port}/predict', json=census_entry)
         print(response.status_code)
         print(response.json())
-
 
 @pytest.mark.skipif(os.environ.get("TEST_REMOTE_API") == None, reason="TEST_REMOTE_API not defined")
 def test_predict_right(census_entries, host, port):
@@ -42,3 +42,30 @@ def test_print_out(census_entries, host, port):
         assert response.status_code == 200
         result_call = response.json()
         assert "age" in result_call
+
+@pytest.mark.skipif(os.environ.get("TEST_REMOTE_API") == None, reason="TEST_REMOTE_API not defined")
+def test_print_out_wrong(wrong_census_entries, host, port):
+    for census_entry in wrong_census_entries:
+        response = requests.post(f'http://{host}:{port}/print_out', json=census_entry)
+        assert response.status_code != 200
+
+
+@pytest.mark.skipif(os.environ.get("TEST_REMOTE_API") == None, reason="TEST_REMOTE_API not defined")
+def test_printout_random(generate_entries, host, port):
+    for generated_entry in generate_entries:
+        print(generated_entry)
+        response = requests.post(f'http://{host}:{port}/print_out', json=generated_entry)
+        print(response.content)
+        assert response.status_code == 200
+        result_call = response.json()
+        assert "age" in result_call
+
+@pytest.mark.skipif(os.environ.get("TEST_REMOTE_API") == None, reason="TEST_REMOTE_API not defined")
+def test_predict_random(generate_entries, host, port):
+    for generated_entry in generate_entries:
+        print(generated_entry)
+        response = requests.post(f'http://{host}:{port}/predict', json=generated_entry)
+        print(response.content)
+        assert response.status_code == 200
+        result_call = response.json()
+        assert result_call["result"].lower() in ["<=50k", ">50k"]
