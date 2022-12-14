36c42dac8880d6e023fdcdcdf8a760abc4bd309e
Diego Amicabile <diego.amicabile@gmail.com>
2022-12-14 06:47:27 +0100

    Additional test to verify that model behaves reasonably

--- test/test_api.py
+++ test/test_api.py
@@ -1,6 +1,6 @@
 import json
 from fastapi.testclient import TestClient
-from common.fixtures import census_entries, wrong_census_entries, generate_entries
+from common.fixtures import generate_entry, census_entries, wrong_census_entries
 
 
 from api.main import app
@@ -14,13 +14,11 @@ def test_get(census_entries):
     result_call = r.json()["result"]
     assert "Welcome" in result_call
 
-
 def test_print_out(census_entries):
     for census_entry in census_entries:
         r = client.post("/print_out", json=census_entry)
         result_call = r.json()
         assert "age" in result_call
-
 
 
 def test_predict_right(census_entries):
@@ -34,3 +32,19 @@ def test_predict_wrong(wrong_census_entries):
     for wrong_census_entry in wrong_census_entries:
         r = client.post("/predict", json=wrong_census_entry)
         assert r.status_code != 200
+
+def test_generate_entries():
+    results = [0, 0]
+    for i in range(100):
+        gen_entry = generate_entry()
+        r = client.post("/predict", json=gen_entry)
+        result_call = r.json()
+        pred = result_call["result"].lower()
+        if pred == ">50k":
+            results[1] += 1
+        elif pred == "<=50k":
+            results[0] += 1
+        else:
+            print(pred)
+    assert results[0] + results[1] == 100
+    assert results[1] < results[0]
